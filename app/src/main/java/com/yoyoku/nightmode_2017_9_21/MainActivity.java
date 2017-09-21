c6ce53a04a8b9a2c85698997edb9eedced62db43
yoyoku <826806125@qq.com>
2017-09-21 16:29:52 +0800

    夜间模式初次提交##测试ssh

---
+++ app/src/main/java/com/yoyoku/nightmode_2017_9_21/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yoyoku.nightmode_2017_9_21;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
